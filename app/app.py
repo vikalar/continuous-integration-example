54966efebfc78eddcc3dd14feb5c8915877eebc2
vikalar <66520658+vikalar@users.noreply.github.com>
2023-04-20 13:33:40 +0200

    Update app.py

--- app/app.py
+++ app/app.py
@@ -2,7 +2,7 @@ from flask import Flask, request, render_template
 import redis
 
 app = Flask(__name__)
-default_key = '1'
+default_key = '1
 cache = redis.StrictRedis(host='redis', port=6379, db=0)
 cache.set(default_key, "one")
 
